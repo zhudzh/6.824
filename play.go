cf87db16bd801bcaa1650849387e679153c6461a
colin <colin2328@gmail.com>
2014-02-10 13:24:11 -0500

    changed play.go

--- play.go
+++ play.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 // import "regexp"
-import "strconv"
-import "reflect"
+// import "strconv"
+// import "reflect"
 
 
 func main() {
-	str := "1"
-	num, _ := strconv.Atoi(str)
-	fmt.Println(reflect.TypeOf(string(num)))
-	fmt.Println(num)
-	fmt.Println(strconv.Itoa(num))
+	// str := "1"
+	// num, _ := strconv.Atoi(str)
+	// fmt.Println(reflect.TypeOf(string(num)))
+	// fmt.Println(num)
+	// fmt.Println(strconv.Itoa(num))
+	operation := "Map"
+	switch operation {
+  case "Map":
+  	fmt.Println("got here")
+  case "Reduce":
+  }
 }
