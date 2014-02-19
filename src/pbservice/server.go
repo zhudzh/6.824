d8dc7265127a6d0a1e98c0db679ee8e412c2272f
colin <colin2328@gmail.com>
2014-02-18 19:39:46 -0500

    change format

--- src/pbservice/server.go
+++ src/pbservice/server.go
@@ -14,121 +14,125 @@ import "sync"
 //import "strconv"
 
 // Debugging
-const Debug = 0
+const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-  if Debug > 0 {
-    n, err = fmt.Printf(format, a...)
-  }
-  return
+	if Debug > 0 {
+		n, err = fmt.Printf(format, a...)
+	}
+	return
 }
 
 type PBServer struct {
-  l net.Listener
-  dead bool // for testing
-  unreliable bool // for testing
-  me string
-  vs *viewservice.Clerk
-  done sync.WaitGroup
-  finish chan interface{}
-  // Your declarations here.
+	l net.Listener
+	dead bool // for testing
+	unreliable bool // for testing
+	me string
+	vs *viewservice.Clerk
+	done sync.WaitGroup
+	finish chan interface{}
+	// Your declarations here.
 }
 
 func (pb *PBServer) Put(args *PutArgs, reply *PutReply) error {
-  // Your code here.
-  return nil
+	// Your code here.
+	return nil
 }
 
 func (pb *PBServer) Get(args *GetArgs, reply *GetReply) error {
-  // Your code here.
-  return nil
+	// Your code here.
+	return nil
 }
 
 
 // ping the viewserver periodically.
 func (pb *PBServer) tick() {
-  // Your code here.
+	fmt.Println(pb.vs)
+	// ok := call(ck.server, "ViewServer.Ping", args, &reply)
+	// if ok == false {
+	// 	return View{}, fmt.Errorf("Ping(%v) failed", viewnum)
+	// }
 }
 
 
 // tell the server to shut itself down.
 // please do not change this function.
 func (pb *PBServer) kill() {
-  pb.dead = true
-  pb.l.Close()
+	pb.dead = true
+	pb.l.Close()
 }
 
 
 func StartServer(vshost string, me string) *PBServer {
-  pb := new(PBServer)
-  pb.me = me
-  pb.vs = viewservice.MakeClerk(me, vshost)
-  pb.finish = make(chan interface{})
-  // Your pb.* initializations here.
+	pb := new(PBServer)
+	pb.me = me
+	pb.vs = viewservice.MakeClerk(me, vshost)
+	pb.finish = make(chan interface{})
+	// Your pb.* initializations here.
 
-  rpcs := rpc.NewServer()
-  rpcs.Register(pb)
+	rpcs := rpc.NewServer()
+	rpcs.Register(pb)
 
-  os.Remove(pb.me)
-  l, e := net.Listen("unix", pb.me);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  pb.l = l
+	os.Remove(pb.me)
+	l, e := net.Listen("unix", pb.me);
+	if e != nil {
+		log.Fatal("listen error: ", e);
+	}
+	pb.l = l
 
-  // please do not change any of the following code,
-  // or do anything to subvert it.
+	// please do not change any of the following code,
+	// or do anything to subvert it.
 
-  go func() {
-    for pb.dead == false {
-      conn, err := pb.l.Accept()
-      if err == nil && pb.dead == false {
-        if pb.unreliable && (rand.Int63() % 1000) < 100 {
-          // discard the request.
-          conn.Close()
-        } else if pb.unreliable && (rand.Int63() % 1000) < 200 {
-          // process the request but force discard of reply.
-          c1 := conn.(*net.UnixConn)
-          f, _ := c1.File()
-          err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
-          if err != nil {
-            fmt.Printf("shutdown: %v\n", err)
-          }
-          pb.done.Add(1)
-          go func() {
-            rpcs.ServeConn(conn)
-            pb.done.Done()
-          }()
-        } else {
-          pb.done.Add(1)
-          go func() {
-            rpcs.ServeConn(conn)
-            pb.done.Done()
-          }()
-        }
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && pb.dead == false {
-        fmt.Printf("PBServer(%v) accept: %v\n", me, err.Error())
-        pb.kill()
-      }
-    }
-    DPrintf("%s: wait until all request are done\n", pb.me)
-    pb.done.Wait() 
-    // If you have an additional thread in your solution, you could
-    // have it read to the finish channel to hear when to terminate.
-    close(pb.finish)
-  }()
+	go func() {
+		for pb.dead == false {
+			conn, err := pb.l.Accept()
+			if err == nil && pb.dead == false {
+				if pb.unreliable && (rand.Int63() % 1000) < 100 {
+					// discard the request.
+					conn.Close()
+				} else if pb.unreliable && (rand.Int63() % 1000) < 200 {
+					// process the request but force discard of reply.
+					c1 := conn.(*net.UnixConn)
+					f, _ := c1.File()
+					err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
+					if err != nil {
+						fmt.Printf("shutdown: %v\n", err)
+					}
+					pb.done.Add(1)
+					go func() {
+						rpcs.ServeConn(conn)
+						pb.done.Done()
+					}()
+				} else {
+					pb.done.Add(1)
+					go func() {
+						rpcs.ServeConn(conn)
+						pb.done.Done()
+					}()
+				}
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && pb.dead == false {
+				fmt.Printf("PBServer(%v) accept: %v\n", me, err.Error())
+				pb.kill()
+			}
+		}
+		DPrintf("%s: wait until all request are done\n", pb.me)
+		pb.done.Wait() 
+		// If you have an additional thread in your solution, you could
+		// have it read to the finish channel to hear when to terminate.
+		close(pb.finish)
+	}()
 
-  pb.done.Add(1)
-  go func() {
-    for pb.dead == false {
-      pb.tick()
-      time.Sleep(viewservice.PingInterval)
-    }
-    pb.done.Done()
-  }()
+	pb.done.Add(1)
+	go func() {
+		for pb.dead == false {
+			pb.tick()
+			time.Sleep(viewservice.PingInterval)
+		}
+		pb.done.Done()
+	}()
 
-  return pb
+	return pb
 }
